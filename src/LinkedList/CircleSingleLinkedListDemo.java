d87cd15aa01dcc532b08b99144914e606cf1ef83
dyxhhxx <15896883188@163.com>
2021-09-13 09:42:06 +0800

    test

--- src/LinkedList/CircleSingleLinkedListDemo.java
+++ src/LinkedList/CircleSingleLinkedListDemo.java
@@ -5,136 +5,137 @@ import org.w3c.dom.css.CSSRuleList;
 
 import java.util.Scanner;
 
+//Josephu环问题
 public class CircleSingleLinkedListDemo {
 
     public static void main(String[] args) {
-        CircleSingleLinkedList cssl=new CircleSingleLinkedList();
+        CircleSingleLinkedList cssl = new CircleSingleLinkedList();
 //        cssl.addNode(5);
 //        cssl.showList();
 //        System.out.printf("小孩个数是：%d\n",cssl.getSize());
 //        System.out.println("出圈顺序：");
 //        cssl.JosephuMethod(1,2);
 
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("请输入小孩个数：");
-        int size=s.nextInt();
+        int size = s.nextInt();
         System.out.println("请输入开始位置：");
-        int start=s.nextInt();
+        int start = s.nextInt();
         System.out.println("请输入报数个数：");
-        int length=s.nextInt();
-        CircleSingleLinkedList cssl1=new CircleSingleLinkedList();
+        int length = s.nextInt();
+        CircleSingleLinkedList cssl1 = new CircleSingleLinkedList();
         cssl1.addNode(size);
         cssl1.showList();
         System.out.println("---------Josephu----------");
-        cssl1.JosephuMethod(start,length);
+        cssl1.JosephuMethod(start, length);
 
     }
 }
 
 //创建环形单向链表
-class CircleSingleLinkedList{
+class CircleSingleLinkedList {
     Boy first;
 
     //添加节点
-    public void addNode(int num){
-        if(num<1){
+    public void addNode(int num) {
+        if (num < 1) {
             System.out.println("boy数量必须大于1");
         }
-        Boy cur=null;
-        for(int i=1;i<=num;i++){
-            Boy boy=new Boy(i);
-            if(i==1){
-                first=boy;
-                first.next=first;
-                cur=first;
-            }
-            else{
-                cur.next=boy;
-                boy.next=first;
-                cur=boy;  //最后指针移动
+        Boy cur = null;
+        for (int i = 1; i <= num; i++) {
+            Boy boy = new Boy(i);
+            if (i == 1) {
+                first = boy;
+                first.next = first;
+                cur = first;
+            } else {
+                cur.next = boy;
+                boy.next = first;
+                cur = boy;  //最后指针移动
             }
         }
     }
 
     //遍历环形链表
-    public void showList(){
-        if(first==null){
+    public void showList() {
+        if (first == null) {
             System.out.println("链表为空");
             return;
         }
-        Boy cur=first;
-        while(true){
+        Boy cur = first;
+        while (true) {
             System.out.println(cur.no);
-            if(cur.next==first){
+            if (cur.next == first) {
                 break;
             }
-            cur=cur.next;
+            cur = cur.next;
         }
     }
 
     //链表长度，但完全没必要，因为
-    public int getSize(){
-        if(first==null){
+    public int getSize() {
+        if (first == null) {
             return 0;
         }
-        Boy temp=first;
-        int sum=0;
-        while(true){
+        Boy temp = first;
+        int sum = 0;
+        while (true) {
             sum++;
-            if(temp.next==first){  //此处不可以用temp.next=temp.next.next代替
+            if (temp.next == first) {  //此处不可以用temp.next=temp.next.next代替
                 break;
             }
-            temp=temp.next;
+            temp = temp.next;
         }
         return sum;
     }
 
     //Josephu出圈方法
-    public void JosephuMethod(int start,int length){
-        if(first==null||start>getSize()||start<1){
+    public void JosephuMethod(int start, int length) {
+        if (first == null || start > getSize() || start < 1) {
             System.out.println("开始位置不在正常范围内，请重新设置start位置");
             return;
         }
-        Boy temp=first;
+        Boy temp = first;
         //让temp指向first的上一个位置，便于之后操作
-        while(true){
-            if(temp.next==first){
+        while (true) {
+            if (temp.next == first) {
                 break;
             }
-            temp=temp.next;
+            temp = temp.next;
         }
         //start=1时temp不需要移动
         //temp移动时，可以让first也移动，这样链表可以在之后继续操作，但如果只是满足Josephu出圈，只用first前但temp就好
-        if(start>1) {
+        if (start > 1) {
             for (int i = 2; i <= start; i++) {
                 temp = temp.next;
 //                first=first.next;
             }
         }
-        while(true){
-            if(temp.next==temp.next.next){
+        while (true) {
+            if (temp.next == temp.next.next) {
                 System.out.println(temp.no);
                 break;
             }
             //让temp向后移动length-1的位置
-            for(int i=0;i<length-1;i++){
-                temp=temp.next;
+            for (int i = 0; i < length - 1; i++) {
+                temp = temp.next;
 //                first=first.next;
             }
             //找出temp下一个位置的no，并使其出链表
             System.out.println(temp.next.no);
 //            first=first.next;
-            temp.next=temp.next.next;
+            temp.next = temp.next.next;
         }
     }
 
 }
 
-class Boy{
+class Boy {
     int no;
     Boy next;
-    public Boy(int no){
-        this.no=no;
+
+    public Boy(int no) {
+        this.no = no;
     }
 
 }
