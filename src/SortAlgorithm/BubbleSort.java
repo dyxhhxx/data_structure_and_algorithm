9a5edd0420975998393c72d26c3fb45b8db33fd3
dingyx <15896883188@163.com>
2021-11-19 15:01:41 +0800

    test

--- src/SortAlgorithm/BubbleSort.java
+++ src/SortAlgorithm/BubbleSort.java
@@ -5,36 +5,36 @@ import java.util.Arrays;
 
 public class BubbleSort {
 
-    public static void Bubble(int[] numlist){
-        for(int i=numlist.length-1;i>0;i--){
+    public static void Bubble(int[] numlist) {
+        for (int i = numlist.length - 1; i > 0; i--) {
             //冒泡排序标志符，为true时说明为排序部分已经按照正序，可以退出
-            boolean flag=true;
-            for(int j=0;j<i;j++){
+            boolean flag = true;
+            for (int j = 0; j < i; j++) {
                 int x;
-                if(numlist[j]>numlist[j+1]){
-                    x=numlist[j];
-                    numlist[j]=numlist[j+1];
-                    numlist[j+1]=x;
-                    flag=false;
+                if (numlist[j] > numlist[j + 1]) {
+                    x = numlist[j];
+                    numlist[j] = numlist[j + 1];
+                    numlist[j + 1] = x;
+                    flag = false;
                 }
             }
-            if(flag){
+            if (flag) {
                 break;
             }
         }
     }
 
     //没有flag的Bubble方法
-    public static void Bubble1(int[] numlist){
-        for(int i=numlist.length-1;i>0;i--){
+    public static void Bubble1(int[] numlist) {
+        for (int i = numlist.length - 1; i > 0; i--) {
             //冒泡排序标志符，为true时说明为排序部分已经按照正序，可以退出
 //            boolean flag=true;
-            for(int j=0;j<i;j++){
+            for (int j = 0; j < i; j++) {
                 int x;
-                if(numlist[j]>numlist[j+1]){
-                    x=numlist[j];
-                    numlist[j]=numlist[j+1];
-                    numlist[j+1]=x;
+                if (numlist[j] > numlist[j + 1]) {
+                    x = numlist[j];
+                    numlist[j] = numlist[j + 1];
+                    numlist[j + 1] = x;
 //                    flag=false;
                 }
             }
@@ -45,44 +45,44 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int[] numlist={1,3,2,4,5};
-        int[] numlist1={3,9,-1,10,-2};
-        int[] numlist2=new int[10000];
-        int[] numlist3=new int[10000];
+        int[] numlist = {1, 3, 2, 4, 5};
+        int[] numlist1 = {3, 9, -1, 10, -2};
+        int[] numlist2 = new int[80000];
+        int[] numlist3 = new int[80000];
         long start;
         long end;
         Bubble(numlist1);
         Bubble(numlist);
-        for(int i:numlist){
-            System.out.print(i+" ");
+        for (int i : numlist) {
+            System.out.print(i + " ");
         }
         System.out.println();
-        for(int i:numlist1) {
+        for (int i : numlist1) {
             System.out.print(i + " ");
         }
         //用运行时间说明，添加flag后程序运行时间有所减短
         //注意：增强型for循环只能遍历，不能给数组赋值
-        for(int i=0;i<numlist2.length;i++){
-            int j=(int)(Math.random()*100000);
-            numlist2[i]=j;
-            numlist3[i]=j;
+        for (int i = 0; i < numlist2.length; i++) {
+            int j = (int) (Math.random() * 100000);
+            numlist2[i] = j;
+            numlist3[i] = j;
         }
-        start=System.currentTimeMillis();
+        start = System.currentTimeMillis();
         Bubble(numlist2);
-        end=System.currentTimeMillis();
-        System.out.println(end-start);
-        start=System.currentTimeMillis();
+        end = System.currentTimeMillis();
+        System.out.println(end - start);
+        start = System.currentTimeMillis();
         Bubble1(numlist3);
-        end=System.currentTimeMillis();
-        System.out.println(end-start);
+        end = System.currentTimeMillis();
+        System.out.println(end - start);
 //        System.out.println(Arrays.toString(numlist2));
-        for(int i:numlist2){
-            System.out.print(i+" ");
-        }
-        System.out.println();
-        for(int i:numlist3){
-            System.out.print(i+" ");
-        }
+//        for(int i:numlist2){
+//            System.out.print(i+" ");
+//        }
+//        System.out.println();
+//        for(int i:numlist3){
+//            System.out.print(i+" ");
+//        }
     }
 
 }
---
+++ src/SortAlgorithm/SelectSort.java
@@ -0,0 +1,70 @@
+package SortAlgorithm;
+
+import javax.swing.plaf.SliderUI;
+
+public class SelectSort {
+
+    //选择排序1，从第一个开始与后面比较，如果更小就直接交换
+    public static void Select1(int[] numarr) {
+        int x;
+        for (int i = 0; i < numarr.length; i++) {
+            for (int j = i + 1; j < numarr.length; j++) {
+                if (numarr[j] < numarr[i]) {
+                    x = numarr[i];
+                    numarr[i] = numarr[j];
+                    numarr[j] = x;
+                }
+            }
+        }
+    }
+
+    //选择排序2，从头至尾找到最小元素的大小和下表，然后和头部元素交换
+    //同样80000个数据，借助index遍历的方法（1000+ms），比直接交换的方法（7000+ms）节省大量时间
+    public static void Select2(int[] numarr) {
+        int index;
+        int num;
+        int x;
+        for (int i = 0; i < numarr.length; i++) {
+            index = i;
+            num = numarr[i];
+            for (int j = i; j < numarr.length; j++) {
+                if (numarr[j] < num) {
+                    index = j;
+                    num = numarr[j];
+                }
+            }
+            //将index处的元素与i处的元素交换
+            x = numarr[index];
+            numarr[index] = numarr[i];
+            numarr[i] = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numarr = new int[80000];
+        int[] numarr1 = new int[80000];
+        long start;
+        long end;
+        for (int i = 0; i < numarr.length; i++) {
+            int j = (int) (Math.random() * 1000000);
+            numarr[i] = j;
+            numarr1[i] = j;
+        }
+        start = System.currentTimeMillis();
+        Select1(numarr);
+        end = System.currentTimeMillis();
+        System.out.println(end - start);
+//        for(int i:numarr){
+//            System.out.print(i+" ");
+//        }
+//        System.out.println();
+        start = System.currentTimeMillis();
+        Select2(numarr1);
+        end = System.currentTimeMillis();
+        System.out.println(end - start);
+//        for(int i:numarr1){
+//            System.out.print(i+" ");
+//        }
+    }
+
+}
