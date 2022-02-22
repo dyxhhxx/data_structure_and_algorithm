3458f3b6ed76398610a0da97835de24b6b3db4f0
dingyx <15896883188@163.com>
2022-02-22 17:10:34 +0800

    test

---
+++ src/Graph/Graph.java
@@ -0,0 +1,132 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Graph {
+
+    private ArrayList<String> vertexList;  //存储顶点集合
+    private int[][] edges;  //存储
+    private int numOfEdges;  //表示边的数目
+    private boolean[] isVisited;
+
+    public static void main(String[] args) {
+        int n = 5;
+        String vertexs[] = {"A", "B", "C", "D", "E"};
+        Graph graph = new Graph(n);
+        //添加顶点
+        for(String value:vertexs){
+            graph.addVertex(value);
+        }
+        //添加边
+        graph.addEdge(0,1,1);
+        graph.addEdge(0,2,1);
+        graph.addEdge(1,2,1);
+        graph.addEdge(1,3,1);
+        graph.addEdge(1,4,1);
+
+        graph.showGraph();
+
+        System.out.println("深度遍历dfs：");
+        graph.dfs();
+    }
+
+    public Graph(int n) {
+        //初始化矩阵和vertexList
+        edges = new int[n][n];
+        vertexList = new ArrayList<String>(n);
+        numOfEdges = 0;
+        isVisited=new boolean[n];
+    }
+
+    //得到第一个邻接节点的下标
+    public int getFirstNeighbor(int index){
+        for(int i=0;i<vertexList.size();i++){
+            if(edges[index][i]>0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //根据前一个邻接节点的下标,获取下一个邻接节点
+    public int getNextNeighbor(int v1,int v2){
+        for(int i=v2+1;i<vertexList.size();i++){
+            if(edges[v1][i]>0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //深度优先dfs遍历算法
+    public void dfs(boolean[] isVisited,int i){
+        System.out.println(getValueByIndex(i)+"->");
+        isVisited[i]=true;  //记录该节点已经被访问过
+        int w = getFirstNeighbor(i);
+        while (w!=-1){  //说明存在邻接节点
+            if(!isVisited[w]){  //说明该邻接节点还没有被访问过
+                dfs(isVisited,w);  //对该邻接节点进行dfs遍历
+            }
+            w=getNextNeighbor(i,w);  //下一个邻接节点（递归终止条件即为：邻接节点值为-1，退出while循环）
+        }
+    }
+
+    //对dfs进行重载,遍历所有节点，并对每个节点进行dfs遍历
+    public void dfs(){
+        for(int i=0;i<vertexList.size();i++){
+            if(!isVisited[i]){
+                dfs(isVisited,i);
+            }
+        }
+    }
+
+    //图中常用的方法
+    //返回节点的个数
+    public int getNumOfVertex() {
+        return vertexList.size();
+    }
+
+    //返回边的个数
+    public int getNumOfEdges() {
+        return numOfEdges;
+    }
+
+    //返回节点i对应的数据 0->A,2->B,3->C...
+    public String getValueByIndex(int i) {
+        return vertexList.get(i);
+    }
+
+    //返回v1和v2的权值
+    public int getWeight(int v1, int v2) {
+        return edges[v1][v2];
+    }
+
+    //显示图对应的矩阵
+    public void showGraph() {
+        for (int[] link : edges) {
+//            for(int i:link){
+//                System.out.println(i+" ");
+//            }
+//            System.out.println();
+            System.out.println(Arrays.toString(link));
+        }
+    }
+
+    //插入顶点
+    public void addVertex(String vertex) {
+        vertexList.add(vertex);
+    }
+
+    //添加边
+    /**
+     * @param v1     点的下标即第几个顶点，即A-0，B-1
+     * @param v2     第二个顶点
+     * @param weight 权值，1表示直接连接，0表示没有直接连接
+     */
+    public void addEdge(int v1, int v2, int weight) {
+        edges[v1][v2] = weight;
+        edges[v2][v1] = weight;
+    }
+
+}
--- src/Tree/AVLTreeDemo.java
+++ src/Tree/AVLTreeDemo.java
@@ -1,4 +1,228 @@
 package Tree;
 
+
+//AVL Tree，平衡二叉树，具有以下特点：它是一颗空树 或 它的左右两个子树的高度差绝对值不超过1，并且左右两个子树都是一棵平衡二叉树
+//AVL Tree查询效率较高
 public class AVLTreeDemo {
+
+    public static void main(String[] args) {
+//        int[] arr={4,3,6,5,7};
+//        Node3 root=new Node3(arr[0]);
+//        for(int i=1;i<arr.length;i++){
+//            root.addNode(new Node3(arr[i]));
+//        }
+//        System.out.println("中序遍历");
+//        root.infixOrder();
+//        System.out.println("前序遍历：");
+//        root.preOrder();
+//
+//        root.addNode(new Node3(8));
+//        System.out.println();
+//        root.infixOrder();
+//        System.out.println();
+//        root.preOrder();
+//
+//        root.leftRotate();
+//        System.out.println("左旋转后：");
+//        System.out.println();
+//        root.infixOrder();
+//        System.out.println();
+//        root.preOrder();
+
+        int[] arr1={10,11,7,6,8,9};
+        Node3 root1=new Node3(arr1[0]);
+        for(int i=1;i<arr1.length;i++){
+            root1.addNode(new Node3(arr1[i]));
+        }
+        System.out.println("中序遍历");
+        root1.infixOrder();
+        System.out.println("前序遍历");
+        root1.preOrder();
+    }
+
+}
+
+class Node3{
+    int value;
+    Node3 left;
+    Node3 right;
+
+    public Node3(int value){
+        this.value=value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setLeft(Node3 left) {
+        this.left = left;
+    }
+
+    public void setRight(Node3 right) {
+        this.right = right;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node3 getLeft() {
+        return left;
+    }
+
+    public Node3 getRight() {
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return "Node3{" +
+                "value=" + value +
+                '}';
+    }
+
+    //中序遍历
+    public void infixOrder(){
+        if(this.left!=null){
+            this.left.infixOrder();
+        }
+        System.out.println(this);
+        if(this.right!=null){
+            this.right.infixOrder();
+        }
+    }
+
+    //前序遍历
+    public void preOrder(){
+        System.out.println(this);
+        if(this.left!=null){
+            this.left.preOrder();
+        }
+        if(this.right!=null){
+            this.right.preOrder();
+        }
+    }
+
+    //添加节点
+    public void addNode(Node3 node){
+        if(this==null){
+            return;
+        }else{
+            if(this.value>node.getValue()){
+                if(this.left==null){
+                    this.left=node;
+                }else{
+                    this.left.addNode(node);
+                }
+            }else{
+                if(this.right==null){
+                    this.right=node;
+                }else{
+                    this.right.addNode(node);
+                }
+            }
+        }
+        this.doubleRotate();
+//        if(rightHeight()-leftHeight()>1){
+//            //分为两种情况：只需要左旋转；需要双旋转
+//            if(right!=null&&right.leftHeight()>right.rightHeight()){  //此时需要进行双旋转
+//                right.rightRotate();
+//                leftRotate();
+//            }else{  //此时只需要进行左旋转
+//                leftRotate();
+//            }
+//            return;
+//        }
+//
+//        if(leftHeight()-rightHeight()>1){
+//            if(left!=null&&left.rightHeight()>left.leftHeight()){
+//                left.leftRotate();
+//                rightRotate();
+//            }else{
+//                rightRotate();
+//            }
+//        }
+    }
+
+    //左旋转方法，旋转后当前接待你仍然是子树的根节点，当前节点的右子节点被舍弃（被新节点代替）
+    public void leftRotate(){
+        //创建新的节点
+        Node3 newNode=new Node3(this.getValue());
+        //把新的节点的左子树设置成当前节点的左子树
+        newNode.setLeft(this.getLeft());
+        //把新的节点的右子树设置成当前节点的右子树的左子树
+        newNode.setRight(this.getRight().getLeft());
+        //把当前节点的值替换成右子节点的值
+        this.setValue(this.getRight().getValue());
+        //把当前节点的左子节点指向新的节点
+        this.setLeft(newNode);
+        //把当前节点的右子节点指向当前节点右子节点的右子节点
+        this.setRight(this.getRight().getRight());
+    }
+
+    //右旋转方法，旋转后当前节点仍然是子树的根节点，当前节点的左子节点被舍弃
+    public void rightRotate(){
+        //创建新的节点
+        Node3 newNode=new Node3(this.getValue());
+        //把新节点的右子树设置成当前节点的右子树
+        newNode.setRight(this.getRight());
+        //新节点的左子树设置成当前节点左子树的右子树
+        newNode.setLeft(this.getLeft().getRight());
+        //把当前节点的值替换成当前节点的左子节点的值
+        this.setValue(this.getLeft().getValue());
+        //把当前节点的左子节点替换成当前节点的左子节点的左子节点
+        this.setLeft(this.getLeft().getLeft());
+        //把当前节点的右子节点替换成新节点
+        this.setRight(newNode);
+    }
+
+    //双旋转（当当前节点的左子节点的右子树高于左子树时进行）
+    //方法：先对当前节点在左子节点进行左旋转，再对当前节点进行右旋转
+    //先写两个方法，获取节点的左右子树高度
+
+    //获取左子树高度
+    public int leftHeight(){
+        if(left==null){
+            return 0;
+        }
+        return left.height();
+    }
+
+    //获取右子树高度
+    public int rightHeight(){
+        if(right==null){
+            return 0;
+        }
+        return right.height();
+    }
+
+    public int height(){
+        return Math.max(left==null?0:left.height(),right==null?0:right.height())+1;  //+1很重要！！！
+    }
+
+    //双旋转实现(该方法是针对递归添加节点，且每添加一次就判断一次的情况，因此该方法实际调用是在回溯时，且一定可以找到非平衡二叉树的最下面节点)
+    public void doubleRotate(){
+        //当添加完一个节点，如果：（右子树的高度-左子树的高度）> 1 ，左旋转
+        if(this.rightHeight()-this.leftHeight()>1){
+            //分为两种情况：只需要左旋转；需要双旋转
+            if(right!=null&&right.leftHeight()>right.rightHeight()){  //此时需要进行双旋转
+                right.rightRotate();
+                this.leftRotate();
+            }else{  //此时只需要进行左旋转
+                this.leftRotate();
+            }
+        }
+
+        if(this.leftHeight()-this.rightHeight()>1){
+            if(left!=null&&left.rightHeight()>left.leftHeight()){
+                left.leftRotate();
+                rightRotate();
+            }else{
+                rightRotate();
+            }
+        }
+    }
+
+
 }
